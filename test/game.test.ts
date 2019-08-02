0265bd4923ecfd6d19ba4334af57b131cb269012
alexcoffee <poxit@hushmail.com>
2019-08-02 13:57:11 -0700

    configured prettier

--- test/game.test.ts
+++ test/game.test.ts
@@ -2,14 +2,14 @@ import { Simulator } from '../src/simulator';
 
 describe('test the odds', () => {
   test('keeping yields 33% win ration', () => {
-    const actual = Simulator.run(false, 10E5);
+    const actual = Simulator.run(false, 10e5);
     const rounded = Math.round(actual * 100);
 
     expect(rounded).toBe(33);
   });
 
   test('swapping yields 66% win ration', () => {
-    const actual = Simulator.run(true, 10E5);
+    const actual = Simulator.run(true, 10e5);
     const rounded = Math.round(actual * 100);
 
     expect(rounded).toBe(67);
